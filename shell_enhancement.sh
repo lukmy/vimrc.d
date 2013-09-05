251aacf65dc4012354bcb9193132a2a8baf1fa47
Yiguang <luyg@ctrip.com>
2013-09-05 13:14:13 +0800

    add a shell enhancement for upgrading vimrc
    just a basic implementation for vimrc_shell_enhancement, no rebase
    failure check, no auto install to shell profile script.
    
    TODOs:
    
    + shell upgrade function
    + rebase failure check
    - auto install to shell profile script
    - important updates remind

---
+++ shell_enhancement.sh
@@ -0,0 +1,18 @@
+
+upgrade_vimrc() {
+    current_dir=$(pwd); cd ~/vimrc.d
+
+    { # try
+        git pull --rebase
+    } || { # catch
+        red_color="\033[31m"
+        default_color="\033[0m"
+        echo ""
+        echo "${red_color}Ooops, it seems that u got problem fetching or merging the repository. go and check it.$default_color"
+        return 
+    }
+
+    vim +BundleInstall +qall
+
+    cd $current_dir
+}
